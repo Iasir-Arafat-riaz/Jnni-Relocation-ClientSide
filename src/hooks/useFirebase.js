6c5dfc65739b09bd14c924f35375ff8de7477388
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2022-01-19 17:26:59 +0600

    registration auth completed

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,20 +1,29 @@
 import React, { useEffect, useState } from "react";
 import FirebaseInitialize from "../firebase/Firebase.Initialize";
-import { getAuth, signInWithPopup, GoogleAuthProvider,onAuthStateChanged,createUserWithEmailAndPassword, signOut } from "firebase/auth";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  createUserWithEmailAndPassword,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 FirebaseInitialize();
 const useFirebase = () => {
-    const [user,setUser]=useState({})
-    const [isLoading,setIsLoading]=useState(true);
-    const [error, setError]=useState("")
+  const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [success,setSuccess]=useState("")
   const auth = getAuth();
   const googleProvider = new GoogleAuthProvider();
 
-  const googleSignIn = (navigate,location) => {
+  const googleSignIn = (navigate, location) => {
     signInWithPopup(auth, googleProvider)
       .then((result) => {
         setUser(result.user);
-        navigate(location?.state?.from||"/Dashboard")
-        setError("")
+        navigate(location?.state?.from || "/Dashboard");
+        setError("");
       })
       .catch((error) => {
         // Handle Errors here
@@ -22,46 +31,70 @@ const useFirebase = () => {
       });
   };
 
-const userRegistration=(email,password,displayName,navigate)=>{
+  const userRegistration = (
+    email,
+    password,
+    displayName,
+    navigate,
+    location
+  ) => {
     createUserWithEmailAndPassword(auth, email, password)
-  .then((result) => {
-    // Signed in 
-    const user = result.user;
-    setError("")
-    // ...
-  })
-  .catch((error) => {
-   
-    setError(error.message);
-    // ..
-  });
-}
-
-  
-  useEffect(()=>{
-    onAuthStateChanged(auth, (user) => {
-        setIsLoading(true)
-        if (user) {
-         setUser(user)
-        } else {
-         setUser({})
-        }
-        setIsLoading(false)
-      });  
-},[])
-  
-
-const logOut=()=>{
-    signOut(auth).then(() => {
-        // Sign-out successful.
-        setError("")
-      }).catch((error) => {
-        setError(error.message)
+      .then((result) => {
+        // Signed in
+        // const user = result.user;
+        const newUser = { email, displayName };
+        setUser(newUser);
+        updateProfile(auth.currentUser, {
+          displayName,
+          email,
+        })
+          .then(() => {
+            // Profile updated!
+            setSuccess("Your Profile Create Successfully")
+            setError("")
+            const destination =location?.state?.from ||"/"
+            navigate(destination)
+            // ...
+          })
+          .catch((error) => {
+            // An error occurred
+            setError(error.message)
+            // ...
+          });
+      })
+      .catch((error) => {
+        setSuccess("")
+        setError(error.message);
+        // ..
       });
-}
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setIsLoading(true);
+      if (user) {
+        setUser(user);
+      } else {
+        setUser({});
+      }
+      setIsLoading(false);
+    });
+  }, []);
+
+  const logOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        setError("");
+        setSuccess("")
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
 
   console.log(user);
-  return {user,googleSignIn,logOut,userRegistration,error,isLoading};
+  return { user, googleSignIn, logOut, userRegistration, error, isLoading,success };
 };
 
 export default useFirebase;
--- src/pages/UserSignin/Register/Register.js
+++ src/pages/UserSignin/Register/Register.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import { Spinner } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
-import useFirebase from "../../../hooks/useFirebase";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+// import useFirebase from "../../../hooks/useFirebase";
+import useAuth from "../../../hooks/useAuth";
 import "./Register.css";
 
 const Register = () => {
   const [userRegister, setUserRegister] = useState({});
   // isLoading, error
-  const { user, userRegistration, isLoading } = useFirebase();
+  const { user, userRegistration, isLoading, success } = useAuth();
   console.log(user);
 
   const navigate = useNavigate();
+  const location = useLocation();
 
   const registerInput = (e) => {
     // console.log(e.target.value);
@@ -30,81 +32,79 @@ const Register = () => {
       alert("password not matched");
       return;
     }
-    userRegister(
+    userRegistration(
       userRegister.email,
       userRegister.password,
       userRegister.displayName,
-      navigate
+      navigate,
+      location
     );
     // setUserRegister({})
     setUserRegister({});
   };
 
   return (
-  
-      <div  className="row userRegister">
-        <div className="col-md-6 col-sm-12 col-12 register-section"></div>
-        <div className="col-md-5  col-sm-12 col-12 ">
-          <div className="register-input">
-            <h3>Please Register Now</h3>
-            <h6 className="text-danger">{/* <b>{error}</b> */}</h6>
-            {/* !isLoading && */}
-            {/* ( */}
-            <form onSubmit={registeration} action="">
-              <input
-                placeholder="Enter Your Name"
-                required
-                onBlur={registerInput}
-                type="text"
-                name="displayName"
-                id=""
-              />
-              <br />
-              <input
-                placeholder="Enter Your Email"
-                required
-                name="email"
-                type="email"
-                onBlur={registerInput}
-              />
-              <br />
-              <input
-                placeholder="Type Your Password"
-                required
-                onBlur={registerInput}
-                type="password"
-                name="password"
-                id=""
-              />
-              <br />
-              <input
-                placeholder="Re-Type Your Password"
-                required
-                onBlur={registerInput}
-                type="password"
-                name="password2"
-                id=""
-              />
-              <br />
-              <input
-                className="register-submit"
-                type="submit"
-                value="Register"
-              />
-            </form>
-            {/* ) */}
-            {isLoading && <Spinner animation="border" variant="info" />}
-            ----------------------
-            <h5>Already Registerd?</h5>
-            <Link className="regLink" to="/login">
-              <h3>
-                <b>Click Here for LOGIN</b>
-              </h3>
-            </Link>
-          </div>
+    <div className="row userRegister">
+      <div className="col-md-7 col-sm-12 col-12 register-section"></div>
+      <div className="col-md-5  col-sm-12 col-12 regMain">
+        <div className="register-input">
+          <h2 className="common"><b>Please Register Now</b></h2>
+          <h6 className="text-danger">{/* <b>{error}</b> */}</h6>
+          {/* !isLoading && */}
+          {/* ( */}
+          <form onSubmit={registeration} action="">
+            <input
+              placeholder="Enter Your Name"
+              required
+              onBlur={registerInput}
+              type="text"
+              name="displayName"
+              id=""
+            />
+            <br />
+            <input
+              placeholder="Enter Your Email"
+              required
+              name="email"
+              type="email"
+              onBlur={registerInput}
+            />
+            <br />
+            <input
+              placeholder="Type Your Password"
+              required
+              onBlur={registerInput}
+              type="password"
+              name="password"
+              id=""
+            />
+            <br />
+            <input
+              placeholder="Re-Type Your Password"
+              required
+              onBlur={registerInput}
+              type="password"
+              name="password2"
+              id=""
+            />
+            <br />
+
+            <input className="register-submit" type="submit" value="Register" />
+          </form>
+          <br />
+          <h6 className="text-success">{success}</h6>
+          {/* ) */}
+          {isLoading && <Spinner animation="border" variant="info" />}
+          ----------------------
+          <h5>Already Registerd?</h5>
+          <Link className="regLink" to="/login">
+            <h4>
+              <b>Click Here for LOGIN</b>
+            </h4>
+          </Link>
         </div>
       </div>
-   
+    </div>
   );
 };
 
